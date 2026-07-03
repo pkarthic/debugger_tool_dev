30f69f66f93364db561c92b98e5f4824c28d46d6
Karthick P <pkarthick@qmaxsys.com>
2026-07-03 18:31:26 +0530

    fix typo in index href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2259057732" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3511346316" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
