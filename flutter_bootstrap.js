8541358faee620a26290ce49452eccc933622905
Karthick P <pkarthick@qmaxsys.com>
2026-07-03 18:19:14 +0530

    increased app version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2454949657" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "528264725" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
